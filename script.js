6e95c247fadfbbe9921d0216f447ea4cf882dfce
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-19 15:10:59 -0500

    oButton click for player

--- script.js
+++ script.js
@@ -13,13 +13,18 @@ const gameBoard = (() => {
   const player2 = Player("Player 2", "");
 
   const xButton = document.getElementById("xBtn");
-    xButton.addEventListener('click', () => {
-        player1.symbol = "X";
-        player2.symbol = "O";
-        currentPlayer = player1;
-    });
+  xButton.addEventListener("click", () => {
+    player1.symbol = "X";
+    player2.symbol = "O";
+    currentPlayer = player1;
+  });
 
-    
+  const oButton = document.getElementById("oBtn");
+  oButton.addEventListener("click", () => {
+    player1.symbol = "O";
+    player2.symbol = "X";
+    currentPlayer = player1;
+  });
 
   cells.forEach((cell) => {
     cell.addEventListener("click", () => {
