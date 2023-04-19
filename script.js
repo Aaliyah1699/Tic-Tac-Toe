b80927444357ad25ece7d1c46b89ad4b219a2c35
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-19 15:41:35 -0500

    board win combos

--- script.js
+++ script.js
@@ -34,6 +34,14 @@ const gameBoard = (() => {
     });
   });
 
+  const checkWin = () => {
+    const winCombo = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+        [0, 4, 8], [2, 4, 6] // diagonal
+    ]
+  }
+
   cells.forEach((cell) => {
     cell.addEventListener("click", () => {
       cell.innerHTML = currentPlayer.getSymbol();
@@ -43,4 +51,8 @@ const gameBoard = (() => {
       currentPlayer = currentPlayer === player1 ? player2 : player1;
     });
   });
+})();
+
+const gameController = (() => {
+
 })();
